6ca34ad87640cff2d3260a66661e40fb46012998
Martin Cabrera <martin.cabrera103@gmail.com>
2023-07-03 03:11:35 -0500

    Added Gameboard Buttons and their CSS properties

---
+++ script.js
@@ -0,0 +1,21 @@
+const Gameboard = (() => {
+    let board = [['X', 'O', 'X'], ['X', 'X', 'X'],['O', 'O', 'O']];
+
+    const displayBoard = () => {
+        for(let i=0; i < 3;i++)
+        {
+            for(let j=0; j < 3;j++)
+            {
+                board[i][j]
+            }
+        }
+    }
+})();
+
+const DisplayController = (() => {
+
+})();
+
+const Player = () => {
+    
+}
